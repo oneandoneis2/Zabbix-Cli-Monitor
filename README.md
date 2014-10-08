61302a0302df95dee50e58de936caae0d0f96125
Dominic Humphries <dominich@broadbean.com>
2014-10-08 15:01:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Zabbix::Cli::Monitor - Blah blah blah
+
+# SYNOPSIS
+
+    use Zabbix::Cli::Monitor;
+
+# DESCRIPTION
+
+Zabbix::Cli::Monitor is
+
+# AUTHOR
+
+Dominic Humphries <dominic@oneandoneis2.com>
+
+# COPYRIGHT
+
+Copyright 2014- Dominic Humphries
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Zabbix/Cli/Monitor.pm
@@ -0,0 +1,39 @@
+package Zabbix::Cli::Monitor;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Zabbix::Cli::Monitor - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Zabbix::Cli::Monitor;
+
+=head1 DESCRIPTION
+
+Zabbix::Cli::Monitor is
+
+=head1 AUTHOR
+
+Dominic Humphries E<lt>dominic@oneandoneis2.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Dominic Humphries
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
